dc86cc70cc8d6dc782b42ff984061fb6fda6369f
xdung04 <157205064+xdung04@users.noreply.github.com>
2025-09-21 10:40:42 +0700

    baitap08

---
+++ ReacJS01/App.jsx
@@ -0,0 +1,11 @@
+import { Cart } from "./components/Cart";
+
+function App() {
+  return (
+    <div className="p-4">
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
---
+++ ReacJS01/index.ts
@@ -0,0 +1,5 @@
+export * from "./components/Button";
+export * from "./components/Input";
+export * from "./components/Card";
+export * from "./components/Modal";
+export * from "./components/Cart";
